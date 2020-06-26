a3ba70e22c271f6d2d1f39e8b38651b39731e29a
Vasyl-007 <vasyl.manyo@gmail.com>
2020-06-26 21:36:27 +0300

    d

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const el = (
-  <div>
-    <h2>Hello React!</h2>
-    <input placeholder="search" />
+const TodoList = () => {
+  return (
     <ul>
       <li>Refresh Reach</li>
       <li>Build Awesome App</li>
     </ul>
-  </div>
-);
-// const el = React.createElement("h2", null, "Hello React Yoohoo!");
-console.log("el", el);
+  );
+};
 
+const AppHeader = () => {
+  return <h2>Task Manager</h2>;
+};
+
+const SearchPanel = () => {
+  return <input placeholder="Search" />;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
@@ -23,7 +38,9 @@ console.log("el", el);
 //   document.getElementById("root")
 // );
 
-ReactDOM.render(el, document.getElementById("root"));
+// const el = React.createElement("h2", null, "Hello React Yoohoo!");
+// console.log("el", el);
+// ReactDOM.render(el, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
