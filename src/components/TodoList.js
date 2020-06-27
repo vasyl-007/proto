6f98edf4acddbcf7788a027d78a3cc7eaf4fd00a
Vasyl-007 <vasyl.manyo@gmail.com>
2020-06-27 14:28:08 +0300

    d

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,18 +2,20 @@ import React from "react";
 import { TodoListItem } from "./TodoListItem";
 
 export const TodoList = ({ todos }) => {
-  const items = ["Refresh React", "Build Awesome App"];
+  const firstElement = (
+    <li>
+      <TodoListItem label={todos[0].label} important={todos[0].important} />
+    </li>
+  );
   return (
-    <ul>
-      <li>
-        <TodoListItem label={todos[0].label} important={todos[0].important} />
-      </li>
+    <div>
+      {firstElement}
       <li>
         <TodoListItem label={todos[1].label} important={todos[1].important} />
       </li>
       <li>
         <TodoListItem label={todos[2].label} important={todos[2].important} />
       </li>
-    </ul>
+    </div>
   );
 };
